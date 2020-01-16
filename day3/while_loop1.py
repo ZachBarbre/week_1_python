9a9ca0d7482c540f80298cbac8d1db88026304ae
Zach Barbre <zach.barbre@gmail.com>
2020-01-16 10:44:26 -0500

    while loop practice

---
+++ day3/while_loop1.py
@@ -0,0 +1,7 @@
+title = "Green Lantern Corp"
+STOP = 10
+count = 0 
+
+while count < STOP:
+    print(count)
+    count += 1
---
+++ day3/while_loop2.py
@@ -0,0 +1,7 @@
+title = "Green Lantern Crop"
+
+count = 0 
+
+while count < len(title):
+    print(count)
+    count += 1
